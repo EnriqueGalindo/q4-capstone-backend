2829e93329c097d8708459437c301f0b75559405
Joseph Brown <joseph.d.brown94@gmail.com>
2020-04-07 13:11:27 -0400

    created user model.

--- user/models.py
+++ user/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class DnDUser(AbstractUser):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
