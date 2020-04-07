209bd98ddc75e061bbcb3144c74cffb26aaa0d26
Joseph Brown <joseph.d.brown94@gmail.com>
2020-04-07 11:53:50 -0400

    Encounters model has been built

--- encounters/models.py
+++ encounters/models.py
@@ -4,3 +4,8 @@ from creatures.models import Creature
 
 class Encounters(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField()
+    creatures = models.ManyToManyField(Creature)
+
+    def __str__(self):
+        return self.title
