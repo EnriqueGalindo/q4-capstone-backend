43de48e81d9ac8065b2a6de620e7d155655a91a8
Joseph Brown <joseph.d.brown94@gmail.com>
2020-04-08 10:24:43 -0400

    viewsets created for all models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from creatures.models import Creature
+from encounters.models import Encounters
+from user.models import DnDUser
+
+from api.serialize import CreatureSerializer, EncountersSerializer, UserSerializer
+
+class CreatureViewSet(viewsets.ModelViewSet):
+    queryset = Creature.objects.all()
+    serializer_class = CreatureSerializer
+
+class EncountersViewSet(viewsets.ModelViewSet):
+    queryset = Encounters.objects.all()
+    serializer_class = EncountersSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = DnDUser.objects.all()
+    serializer_class = UserSerializer
