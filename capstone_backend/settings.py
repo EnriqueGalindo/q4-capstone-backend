660022d97d8c6cea3879e045d50d1791057dc70c
Joseph Brown <joseph.d.brown94@gmail.com>
2020-04-21 15:34:55 -0400

    Added exception handlers

--- capstone_backend/settings.py
+++ capstone_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f!-)(2@#eg&lx0z4t8l#1^@j4cb0a61%&+oi1%r21ble7mrn)l'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
